2df8222e98de047b003c9e1b14dff36b75c62830
muirurimukundi <themasalacollective@gmail.com>
2024-06-25 11:23:01 -0400

    changed path

--- reactPro/src/components /Hello.jsx
+++ reactPro/src/components /Hello.jsx
@@ -1,7 +1,7 @@
 function Hello() {
-    // component name must be same as file name
-    // components must return something, 
-    return <h1>Hello from a component!</h1>
+  // component name must be same as file name
+  // components must return something,
+  return <h1>Hello from a component!</h1>;
 }
 
 export default Hello;
