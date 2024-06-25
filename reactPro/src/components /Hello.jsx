e3ba428a5ee71d4cae57bd32e58ae161761646ff
muirurimukundi <themasalacollective@gmail.com>
2024-06-25 11:42:04 -0400

    Solve bug path name should not start with ./

--- reactPro/src/components /Hello.jsx
+++ reactPro/src/components /Hello.jsx
@@ -1,4 +1,17 @@
+const name = "Tobby";
+
+function Hello() {
+  return (
+    <div className="msg">
+      <h1>Hello Mr.</h1>
+    </div>
+  );
+}
+
+export default Hello;
+
 // function Hello() {
+
 //   // component name must be same as file name
 //   // components must return something,
 //   return <h1>Hello from a component!</h1>;
