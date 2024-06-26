a9d6869c79dd2128936043041ed43c6b18c37ef4
muirurimukundi <themasalacollective@gmail.com>
2024-06-26 10:54:59 -0400

    props makes components dynamic

--- reactPro/src/App.jsx
+++ reactPro/src/App.jsx
@@ -1,11 +1,14 @@
 import Hello from "./components/Hello";
 
+// props makes components dynamic
 function App() {
   return (
     <div className="App">
-      <Hello />
-      <Hello />
-
+      {/* props are passed in a similar way as html attributes */}
+      <Hello name="Rob" />
+      <Hello name="Jessica" />
+      <Hello message="Howdy" name="Mark" />
+      <Hello name="Tom" message="Hello" />
     </div>
   );
 }
--- reactPro/src/components/Hello.jsx
+++ reactPro/src/components/Hello.jsx
@@ -3,11 +3,19 @@ const name = "Tobby";
 function displayMessage() {
   return "Wow";
 }
-function Hello() {
+
+// props makes components dynamic, we make the Hello function below to say hi to different people(names) by making it accept props
+// components are regular fns which return jsx , we pass props as arguments
+// props is like this
+
+function Hello(props) {
+
   // return <h1>Hello from component {displayMessage()}</h1>;
   return (
     <div>
-      <h1>Hello Mike </h1>
+      <h1>Hello Mike {props.name}</h1> 
+      <h1>Hello {props.name}</h1> 
+      <h1>{props.message}  {props.name}</h1>   
     </div>
   );
 }
