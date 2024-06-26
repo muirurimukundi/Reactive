057ccc240ae82a812f84854d6df9c1f79e5a93f8
muirurimukundi <themasalacollective@gmail.com>
2024-06-26 10:41:26 -0400

    Components are reusable, in the app.js file we call the Hello component twice.

--- reactPro/src/components/Hello.jsx
+++ reactPro/src/components/Hello.jsx
@@ -1,12 +1,15 @@
 const name = "Tobby";
 
-
-function displayMessage(){
-
+function displayMessage() {
   return "Wow";
 }
 function Hello() {
-  return <h1>Hello from component {displayMessage()}</h1>;
+  // return <h1>Hello from component {displayMessage()}</h1>;
+  return (
+    <div>
+      <h1>Hello Mike </h1>
+    </div>
+  );
 }
 
 export default Hello;
